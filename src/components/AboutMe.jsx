ad680aaad606977dc4a27a2e4479c7fe4e25cb49
AOA19 <aakinyemi19@gmail.com>
2025-02-21 13:35:37 -0500

    Updated about me paragraph

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,18 +2,19 @@ import Header from "./Header"
 
 const AboutMe = () => {
   return (
-    <div className="mt-12">
+    <div className="mt-24">
       <section id="about">
         <Header title="About Me" />
-        <p className="text-center font-sora text-olive dark:text-linen">
-          I’ve always sought challenging opportunities, starting with earning my degree in Electrical Engineering and working as an electrical design engineer. 
-          In that role, I gained experience designing and integrating electrical systems like power distribution and control systems for various buildings. 
-          My interest in coding began in college when I took an Intro to Programming class and learned C. After working as an electrical engineer, my curiosity 
-          for coding grew, leading me to teach myself JavaScript and build a strong foundation
+        <p className="text-center font-sora text-olive dark:text-linen text-lg min-[560px]:px-6 lg:px-36 leading-8">
+          I’m a software engineer who thrives at the intersection of code and design, 
+          building dynamic web applications. My career began in electrical engineering, 
+          where I gained experience designing and integrating electrical systems for various buildings. 
+          I was first introduced to programming in college through an Intro to Programming course, 
+          which sparked my interest in software development. Over time, that curiosity grew, leading 
+          me to transition into software engineering by teaching myself JavaScript and developing the skills needed to build impactful solutions.
+          Beyond coding, I love baking, playing soccer and tennis, and killing it on the karaoke mic. Whether it’s developing a web application
+         or perfecting a recipe, I enjoy turning ideas into reality.
         </p>
-        <p className="text-center font-sora text-olive dark:text-linen">When I'm not coding, you can find me testing out a new recipe to bake, staying active by playing soccer and tennis, and killing it 
-          on the karaoke mic. I enjoy creating things from conception to production; whether it’s a web application or a new recipe, 
-          I’m always up for the challenge! If you're interested in working together, feel free to get in contact with me below.</p>
       </section>
     </div>
   );
